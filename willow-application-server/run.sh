4c36201663b62b9847fb01542bd943264801bb91
Jack Journeyman <145021130+jackjourneyman@users.noreply.github.com>
2025-05-21 14:04:40 +0100

    Update run.sh

--- willow-application-server/run.sh
+++ willow-application-server/run.sh
@@ -5,9 +5,9 @@ echo "Starting Willow Application Server..."
 # Map volume for persistent storage
 # `/config` is a volume for persistent storage on Home Assistant
 # `/app/storage` is a volume for persistent storage in the container
-# echo "Mapping persistent storage..."
-# rm -rf /app/storage
-# ln -s /config /app/storage
+echo "Mapping persistent storage..."
+rm -rf /app/storage
+ln -s /config /app/storage
 
 
 # Parse configuration options
